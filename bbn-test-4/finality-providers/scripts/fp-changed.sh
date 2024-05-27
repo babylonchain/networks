052750f6f1c1b9dd81a148a05b7f8db006a38acd
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2024-05-27 13:42:21 -0300

    chore: split check tx values on verify off and on chain

---
+++ bbn-test-4/finality-providers/scripts/fp-changed.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eu
+
+# USAGE:
+# ./fp-changed.sh
+
+# returns
+
+if ! git remote get-url bbnRepo &> /dev/null; then
+  git remote add bbnRepo https://github.com/babylonchain/networks.git
+fi
+git fetch bbnRepo main
+
+function fpChanged {
+  git diff --diff-filter=AM bbnRepo/main --name-only **/finality-providers/registry/
+}
--- bbn-test-4/finality-providers/scripts/verify-new-fp-offchain.sh
+++ bbn-test-4/finality-providers/scripts/verify-new-fp-offchain.sh
@@ -1,10 +1,17 @@
 #!/bin/bash -eu
 
 # USAGE:
-# ./verify-new-fp.sh
+# ./verify-new-fp-offchain.sh
 
 # checks for modified files in **/bbn-test-4/finality-providers/** compared to main branch
-# and validates all checks to make sure it is a valid finality provider registration
+# and validates if the finality provider registration has valid values to send the
+# transaction to BTC chain.
+
+CWD="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+. $CWD/fp-changed.sh
+
+EOTSD_BIN="${EOTSD_BIN:-eotsd}"
+STAKERCLI_BIN="${STAKERCLI_BIN:-stakercli}"
 
 if ! command -v jq &> /dev/null; then
   echo "⚠️ jq command could not be found!"
@@ -12,30 +19,25 @@ if ! command -v jq &> /dev/null; then
   exit 1
 fi
 
-if ! command -v eotsd &> /dev/null; then
-  echo "⚠️ eotsd command could not be found!"
+if ! command -v $EOTSD_BIN &> /dev/null; then
+  echo "⚠️ $EOTSD_BIN command could not be found!"
   echo "Install it by checking https://github.com/babylonchain/finality-provider.git"
   exit 1
 fi
 
-if ! command -v stakercli &> /dev/null; then
-  echo "⚠️ stakercli command could not be found!"
+if ! command -v $STAKERCLI_BIN &> /dev/null; then
+  echo "⚠️ $STAKERCLI_BIN command could not be found!"
   echo "Install it by checking https://github.com/babylonchain/btc-staker.git"
   exit 1
 fi
 
-if ! git remote get-url bbnRepo &> /dev/null; then
-  git remote add bbnRepo https://github.com/babylonchain/networks.git
-fi
-git fetch bbnRepo main
-
-ALL_CHANGED_FILES=$(git diff --diff-filter=AM bbnRepo/main --name-only **/finality-providers/registry/)
-if [ ${#ALL_CHANGED_FILES} -lt 1 ]; then
+FP_CHANGED_FILES=$(fpChanged)
+if [ ${#FP_CHANGED_FILES} -lt 1 ]; then
   echo "No new finality provider to register"
   exit 0
 fi
 
-for filePathRegistryFP in ${ALL_CHANGED_FILES}; do
+for filePathRegistryFP in ${FP_CHANGED_FILES}; do
   echo "verify" "$filePathRegistryFP"
 
   moniker=$(cat "$filePathRegistryFP" | jq -r '.description.moniker')
@@ -56,31 +58,15 @@ for filePathRegistryFP in ${ALL_CHANGED_FILES}; do
   echo "fp signature:" $signature
 
   echo "eotsd verify signature"
-  eotsd verify-schnorr-sig "$filePathRegistryFP" --btc-pk $btcPk --signature $signature
+  $EOTSD_BIN verify-schnorr-sig "$filePathRegistryFP" --btc-pk $btcPk --signature $signature
   echo
 
   signedTx=$(cat "$filePathRegistryFP" | jq -r '.deposit.signed_tx')
   echo "stakercli check transaction"
-  stakercli transaction check-phase1-staking-transaction \
+  $STAKERCLI_BIN transaction check-phase1-staking-transaction \
     --covenant-committee-pks 50929b74c1a04954b78b4b6035e97a5e078a5a0f28ec96d547bfee9ace803ac0 --covenant-quorum 1 \
     --magic-bytes 62627434 --network signet --staking-transaction $signedTx --finality-provider-pk $btcPk \
     --staking-time 52560 --min-staking-amount=10000000
 
-  txHash=$(cat "$filePathRegistryFP" | jq -r '.deposit.tx_hash')
-
-  onchainTx=$(curl https://signet.bitcoinexplorer.org/api/tx/$txHash | jq)
-  confirmations=$(echo $onchainTx | jq -r '.confirmations')
-  signedTxOnChain=$(echo $onchainTx | jq -r '.hex')
-
-  echo "BTC check transaction"
-  if [[ "$confirmations" -lt "6" ]]; then
-    echo "The tx ${txHash} has ${confirmations} confirmations, it should have at least 6"
-    exit 1
-  fi
-
-  if [[ $signedTx != $signedTxOnChain ]]; then
-    echo "Signed tx in json ${signedTx} is different than signed tx on signet ${signedTxOnChain}"
-    exit 1
-  fi
   echo "✅ '${nickname}' is a valid fp registration"
 done
