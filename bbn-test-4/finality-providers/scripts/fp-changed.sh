60cc002ab95c1225b4a2137fda24f3f262a05215
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2024-05-30 07:59:06 -0300

    chore: removed unused echo

--- bbn-test-4/finality-providers/scripts/fp-changed.sh
+++ bbn-test-4/finality-providers/scripts/fp-changed.sh
@@ -9,7 +9,6 @@ function fpChanged {
   originUrl=$(git remote get-url origin)
   currBranch=$(git rev-parse --abbrev-ref HEAD)
   if [ "main" == "$currBranch" ] && [[ "$originUrl" =~ .*"babylonchain/networks.git".* ]]; then
-    echo "Merge on main"
     git diff HEAD~1 --name-only **/finality-providers/registry/
   else
     if ! git remote get-url bbnRepo &> /dev/null; then
